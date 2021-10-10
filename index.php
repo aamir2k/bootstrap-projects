c782f23a8d137d9617960371b6d53a4e55de5f23
17PWCSE1557 <17pwcse1557@uetpeshawar.edu.pk>
2021-10-10 23:50:19 +0500

    bootstrap template

---
+++ index.php
@@ -0,0 +1,217 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <script src="https://kit.fontawesome.com/63a22f8162.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>bootStrap</title>
+  </head>
+  <body>
+      <!------------------ heder part -------------->
+      
+      <div id="header">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4" >
+              <h2 id="logo">Company Name</h2>
+            </div>
+            <div class="col-md-8">
+            <ul id="menu" class="float-md-right">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About US</a></li>
+              <li><a href="#">Gallery</a></li>
+              <li><a href="#">Products</a></li>
+              <li><a href="#">Cantact Us</a></li>
+            </ul>
+            </div>
+          </div> 
+        </div>
+      </div>
+      <!--====================== Banner ==============-->
+      <div id="banner">
+      <div class="container">
+        <div class="row">
+          <!-- <img src="images/logo.jpg" alt=""> -->
+          <div class="col-md-8 col-lg-6">
+            <h1 class="title">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, libero?</h1>
+            <p class="leead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae maxime quo officiis laudantium, soluta debitis officia. Sequi porro voluptas distinctio.</p>
+          </div>    
+        </div>
+      </div>
+      </div>
+      <!-- =============Section============= -->
+      <div class="container section">
+        <div class="row">
+          <div class="col-lg-12">
+            <h2 class="section-heading">Some Heading Goes Here</h2>
+          </div>
+          <br class="break-line">
+          <div class="col-lg-4">
+           <div class="service-box">
+             <i class="fa fa-anchor" ></i>
+             <h3>Some heading</h3>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, tempore.</p>
+           </div>
+
+          </div>
+          <div class="col-lg-4">
+          <div class="service-box">
+             <i class="fa fa-bar-chart"></i>
+             <h3>Some heading</h3>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, tempore.</p>
+           </div>
+          </div>
+          <div class="col-lg-4">
+          <div class="service-box">
+             <i class="fa fa-area-chart"></i>
+             <h3>Some heading</h3>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, tempore.</p>
+           </div>
+          </div>
+        </div>
+      </div>
+      <!-- ==================Testimonial=================== -->
+      <div id="testimonial" class="section">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <h2 class="section-heading">Testimonial</h2>
+            </div>
+            <div class="col-md-6">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, labore.</p>
+              <img src="./images/billgates.jpg" alt="bill gates" class="image">
+              <div class="author">Bill Gates</div>
+              <div class="author-company">Microsoft</div>
+            </div>
+            <div class="col-md-6">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, labore.</p>
+              <img src="./images/elanmusk.jpg" alt="elan musk" class="image">
+              <div class="author">Elan Musk</div>
+              <div class="author-company">Xspace</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- ======================Recent News=============== -->
+      <div id="news" class="section">
+        <div class="container">
+          <div class="row">
+          <div class="col-md-12">
+            <h2 class="section-heading">Recent News</h2>
+          </div>
+          <div class="col-lg-3">
+              <div class="news-post">
+                <img src="./images/news---1.jpg" alt="" class="news-images">
+                <h3 class="news-heading"> <a href="#">News Heading here</a></h3>
+                <div class="post-date"> <i class="fa fa-calendar"></i> April-25, 2020</p></div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, ratione?</p>
+                <!-- <button class="button">Read more</button> -->
+                <p class="button"> <a href="#">Read more</a> </p>
+              </div>
+            </div>
+              <div class="col-lg-3">
+              <div class="news-post">
+                <img src="./images/news---1.jpg" alt="" class="news-images">
+                <h3 class="news-heading"> <a href="#">News Heading here</a></h3>
+                <div class="post-date"> <i class="fa fa-calendar"></i> April-25, 2020</p></div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, ratione?</p>
+                <p class="button"> <a href="#">Read more</a> </p>
+              </div>
+              </div>
+              <div class="col-lg-3">
+              <div class="news-post">
+                <img src="./images/news---1.jpg" alt="" class="news-images">
+                <h3 class="news-heading"> <a href="#">News Heading here</a></h3>
+                <div class="post-date"> <i class="fa fa-calendar"></i> April-25, 2020</p></div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, ratione?</p>
+                <p class="button"> <a href="#">Read more</a> </p>
+              </div>
+              </div>
+              <div class="col-lg-3">
+              <div class="news-post">
+                <img src="./images/news---1.jpg" alt="" class="news-images">
+                <h3 class="news-heading"> <a href="#">News Heading here</a></h3>
+                <div class="post-date"> <i class="fa fa-calendar"></i> April-25, 2020</p></div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, ratione?</p>
+                <p class="button"> <a href="#">Read more</a> </p>
+              </div>
+              </div>
+        </div>
+      </div>
+      </div>
+      <!-- =================main footer================ -->
+      <div id="main-footer">
+        <div class="container">
+            <div class="row">
+              <div class="col-md-4">
+                <div class="footer-widght">
+                  <h4 class="widght-heading">About Company</h4>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit iusto vitae necessitatibus beatae sequi est!</p>
+                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="footer-widght">
+                  <h4 class="widght-heading">Latest News</h4>
+                  <ul class="list-style">
+                    <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="footer-widght">
+                  <h4 class="widght-heading">Company Address</h4>
+                  <ul class="list-style">
+                  <li><a href="#">Aizaz baba</a></li>
+                  <li><a href="#">IT park near PTCL center.</a></li>
+                  <li><a href="#">aizazbaba@obashi.com</a></li>
+                  <li><a href="#">0320-4561209</a></li>
+                  </ul>
+                </div>
+              </div>
+              
+            </div>
+        </div>
+      </div>
+      <!-- ========================last-footer=================== -->
+      <div id="last-footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <p>copyright@2021 </p> 
+            </div>
+            <div class="col-md-6">
+              <ul id="social-icons" class="float-md-right">
+                <li><a href="#" class="fa fa-twitter"></a></li>
+                <li><a href="#" class="fa fa-facebook-square"></a></li>
+                <li ><a href="#" class="fa fa-instagram" ></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+        
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
